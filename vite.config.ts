076d3ab13c9068404ebe710ccc11ec2f9cb3eb75
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-10-15 10:05:31 +0500

    ci/cd deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     include: ['tailwind-config']
   },
   plugins: [react(), tsconfigPaths()],
+  base: '/staff-front-v2/',
   resolve: {
     alias: {
       'tailwind-config': path.resolve(__dirname, './tailwind.config.js')
