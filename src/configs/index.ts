67272b5083d8c58f6c1bbde8537753420e9ca7a9
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-11-11 10:24:38 +0500

    add env

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,6 @@
 const config = {
-  API_ROOT: import.meta.env.VITE_APP_NAME ?? 'http://192.168.100.82:3001/',
-  FILE_URL: import.meta.env.VITE_APP_URL ?? 'http://192.168.100.82:3001/',
+  API_ROOT: import.meta.env.VITE_APP_NAME ?? 'http://139.28.47.17:3703/',
+  FILE_URL: import.meta.env.VITE_APP_URL ?? 'http://139.28.47.17:3703/',
   DEFAULT_APP_LANG: 'ru',
   ROLES: [],
   PERMISSIONS: []
