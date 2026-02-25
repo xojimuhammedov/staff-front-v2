126cd0dc8cbd95fd7482c83e5824a16d6f07b8e9
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2026-02-25 09:29:43 +0500

    change employee

--- src/pages/EmployeePage/Create/_components/Form.tsx
+++ src/pages/EmployeePage/Create/_components/Form.tsx
@@ -10,7 +10,7 @@ import { UploadCloud } from 'lucide-react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { object, string } from 'yup';
-import * as yup from "yup";
+import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
@@ -32,18 +32,11 @@ interface Credential {
 function Form() {
   const { t, i18n } = useTranslation();
   const currentLang = i18n.resolvedLanguage;
-  const userData: any = storage.get("userData")
-  const userRole = JSON.parse(userData)?.role
+  const userData: any = storage.get('userData');
+  const userRole = JSON.parse(userData)?.role;
   const [openModal, setOpenModal] = useState(false);
-  const [imageKey, setImageKey] = useState(null)
-  const [credentials, setCredentials] = useState<Credential[]>([
-    {
-      code: '',
-      type: 'QR',
-      additionalDetails: '',
-    },
-  ]);
-  const navigate = useNavigate()
+  const [imageKey, setImageKey] = useState(null);
+  const navigate = useNavigate();
   const [preview, setPreview] = useState<any>();
   const { getProcessedImage, setImage, resetStates }: any = useImageCropContext();
   const handleDone = async (): Promise<void> => {
@@ -57,17 +50,16 @@ function Form() {
           // 🧩 2. API'ga yuboramiz
           const response = request.post(URLS.uploadPhotoByEmployee, formData, {
             headers: {
-              "Content-Type": "multipart/form-data"
-            }
-          })
-          response.then((res) => setImageKey(res?.data?.key))
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+          response.then((res) => setImageKey(res?.data?.key));
 
           const imageUrl = URL.createObjectURL(avatar);
           setPreview(imageUrl);
 
           resetStates();
           setOpenModal(false);
-
         } catch (error) {
           console.error('❌ Error uploading avatar:', error);
         }
@@ -90,58 +82,50 @@ function Form() {
     key: KEYS.getAllListOrganization,
     url: URLS.getAllListOrganization,
     params: {},
-    hideErrorMsg: true
-  })
+    hideErrorMsg: true,
+  });
 
   const { data: jobData } = useGetAllQuery<any>({
     key: KEYS.employeeJobPosition,
     url: URLS.employeeJobPosition,
     params: {},
-    hideErrorMsg: true
-  })
+    hideErrorMsg: true,
+  });
 
   const schema = object().shape({
     name: string().required(),
-    email: yup
-      .string()
-      .transform(v => v === "" ? undefined : v),
-    phone: yup
-      .string()
-      .transform(v => v === "" ? undefined : v),
-    address: yup
-      .string()
-      .transform(v => v === "" ? undefined : v),
+    email: yup.string().transform((v) => (v === '' ? undefined : v)),
+    phone: yup.string().transform((v) => (v === '' ? undefined : v)),
+    address: yup.string().transform((v) => (v === '' ? undefined : v)),
     departmentId: yup
       .number()
       .when('$role', (role: any, schema) =>
-        role === 'ADMIN' || "HR" ? schema.required() : schema.optional()
+        role === 'ADMIN' || 'HR' ? schema.required() : schema.optional()
       ),
     organizationId: yup
       .number()
       .when('$role', (role: any, schema) =>
         role === 'ADMIN' ? schema.required() : schema.optional()
       ),
-    additionalDetails: yup
-      .string()
-      .transform(v => v === "" ? undefined : v),
-    jobId: yup.number().required()
+    additionalDetails: yup.string().transform((v) => (v === '' ? undefined : v)),
+    jobId: yup.number().required(),
   });
   const {
     handleSubmit,
     register,
     control,
     watch,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     defaultValues: {},
     mode: 'onChange',
     resolver: yupResolver(schema),
-    context: { role: userRole }
+    context: { role: userRole },
   });
 
   const { mutate: create } = usePostQuery({
     listKeyId: KEYS.getEmployeeList,
-    hideSuccessToast: true
+    hideSuccessToast: true,
   });
 
   const onSubmit = (data: any) => {
@@ -150,87 +134,60 @@ function Form() {
         url: URLS.getEmployeeList,
         attributes: {
           photo: imageKey,
-          // credentials: credentials,
-          ...data
-        }
+          ...data,
+        },
       },
       {
         onSuccess: () => {
           toast.success(t('Successfully created!'));
-          navigate('/employees')
+          navigate('/employees');
         },
         onError: (e: any) => {
-          toast.error(e?.response?.data?.error?.message)
-        }
+          toast.error(e?.response?.data?.error?.message);
+        },
       }
     );
   };
-
-  const addCredential = () => {
-    setCredentials([
-      ...credentials,
-      {
-        code: '',
-        type: 'QR',
-        additionalDetails: '',
-      },
-    ]);
-  };
-
-  const removeCredential = (index: number) => {
-    if (credentials.length === 1) {
-      alert("Kamida bitta hujjat qoldirish kerak!");
-      return;
-    }
-    setCredentials(credentials.filter((_, i) => i !== index));
-  };
-
-  const updateCredential = (index: number, field: keyof Credential, value: any) => {
-    const updated = [...credentials];
-    updated[index] = { ...updated[index], [field]: value };
-    setCredentials(updated);
-  };
-
 
   const { data: getDepartment } = useGetAllQuery<any>({
     key: KEYS.getAllListDepartment,
     url: URLS.getAllListDepartment,
     params: {
-      organizationId: watch("organizationId")
-    }
-  })
+      organizationId: watch('organizationId'),
+    },
+  });
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='sm:w-full lg:w-2/3 flex gap-6 justify-between'>
-          <div className='grid grid-cols-2 gap-4 w-3/4'>
+        <div className="sm:w-full lg:w-2/3 flex gap-6 justify-between">
+          <div className="grid grid-cols-2 gap-4 w-3/4">
             <MyInput
-              {...register("name")}
+              {...register('name')}
               error={Boolean(errors?.name?.message)}
               helperText={t(`${errors?.name?.message}`)}
               label={t('Employee name')}
             />
             <MyInput
-              {...register("address")}
+              {...register('address')}
               error={Boolean(errors?.address?.message)}
               helperText={t(`${errors?.address?.message}`)}
               label={t('Employee address')}
             />
             <MyInput
-              {...register("phone")}
+              {...register('phone')}
               error={Boolean(errors?.phone?.message)}
               helperText={t(`${errors?.phone?.message}`)}
               label={t('Employee phone number')}
             />
             <MyInput
-              {...register("email")}
+              {...register('email')}
               error={Boolean(errors?.email?.message)}
               helperText={t(`${errors?.email?.message}`)}
               label={t('Employee email')}
             />
             <MyInput
-              {...register("additionalDetails")}
+              {...register('additionalDetails')}
               error={Boolean(errors?.additionalDetails?.message)}
               helperText={t(`${errors?.additionalDetails?.message}`)}
               label={t('Employee details')}
@@ -240,16 +197,16 @@ function Form() {
               control={control}
               render={({ field, fieldState }) => (
                 <MySelect
-                  label={t("Select organization")}
+                  label={t('Select organization')}
                   options={data?.data?.map((evt: any) => ({
                     label: evt.fullName,
                     value: evt.id,
                   }))}
-                  value={field.value as any}  // 👈 cast to any
+                  value={field.value as any} // 👈 cast to any
                   onChange={(val) => field.onChange(Number((val as ISelect)?.value ?? val))}
                   onBlur={field.onBlur}
                   error={!!fieldState.error}
-                  allowedRoles={["ADMIN"]}
+                  allowedRoles={['ADMIN']}
                 />
               )}
             />
@@ -258,16 +215,16 @@ function Form() {
               control={control}
               render={({ field, fieldState }) => (
                 <MySelect
-                  label={t("Select department")}
-                  options={get(getDepartment, "data")?.map((evt: Department) => ({
+                  label={t('Select department')}
+                  options={get(getDepartment, 'data')?.map((evt: Department) => ({
                     label: evt.fullName,
                     value: evt.id,
                   }))}
-                  value={field.value as any}  // 👈 cast to any
+                  value={field.value as any} // 👈 cast to any
                   onChange={(val) => field.onChange(Number((val as ISelect)?.value ?? val))}
                   onBlur={field.onBlur}
                   error={!!fieldState.error}
-                  allowedRoles={["ADMIN", "HR"]}
+                  allowedRoles={['ADMIN', 'HR']}
                 />
               )}
             />
@@ -276,16 +233,16 @@ function Form() {
               control={control}
               render={({ field, fieldState }) => (
                 <MySelect
-                  label={t("Select position")}
-                  options={get(jobData, "items")?.map((evt: any) => ({
+                  label={t('Select position')}
+                  options={get(jobData, 'items')?.map((evt: any) => ({
                     label: evt[`${currentLang}`],
                     value: evt.id,
                   }))}
-                  value={field.value as any}  // 👈 cast to any
+                  value={field.value as any} // 👈 cast to any
                   onChange={(val) => field.onChange(Number((val as ISelect)?.value ?? val))}
                   onBlur={field.onBlur}
                   error={!!fieldState.error}
-                  allowedRoles={["ADMIN", "HR"]}
+                  allowedRoles={['ADMIN', 'HR']}
                 />
               )}
             />
@@ -308,55 +265,26 @@ function Form() {
             />
             <label
               htmlFor="avatarInput"
-              className="mt-6 flex h-[32px] cursor-pointer items-center justify-center gap-2 rounded-md border border-solid border-gray-300 px-[6px] py-[6px]  text-xs font-medium text-gray-700 shadow-sm dark:text-text-title-dark">
+              className="mt-6 flex h-[32px] cursor-pointer items-center justify-center gap-2 rounded-md border border-solid border-gray-300 px-[6px] py-[6px]  text-xs font-medium text-gray-700 shadow-sm dark:text-text-title-dark"
+            >
               <UploadCloud /> {t('Upload image')}
             </label>
           </div>
         </div>
-        {/* {
-          credentials?.map((item, index) => (
-            <div className='flex items-center gap-4 w-3/4'>
-              <div key={index} className='grid grid-cols-3 gap-3 mt-4 w-full'>
-                <MySelect
-                  label={t("Select type")}
-                  options={typeData?.map((evt: any) => ({
-                    label: evt.label,
-                    value: evt.value,
-                  }))}
-                  value={item.type}
-                  onChange={(val) => updateCredential(index, 'type', ((val as ISelect)?.value ?? val) as Credential['type'])}
-                  allowedRoles={["ADMIN", "HR"]}
-                />
-                <MyInput
-                  value={item?.code}
-                  onChange={(e) => updateCredential(index, 'code', e.target.value)}
-                  label={t('Code')}
-                />
-                <MyInput
-                  value={item?.additionalDetails}
-                  onChange={(e) => updateCredential(index, 'additionalDetails', e.target.value)}
-                  label={t('Add information')}
-                />
-              </div>
-              <MyButton startIcon={<Trash2 size={DEFAULT_ICON_SIZE} />} type='button' className={'border p-2 mt-10'} onClick={() => removeCredential(index)} />
-            </div>
-          ))
-        } */}
-        {/* <MyButton startIcon={<Plus size={DEFAULT_ICON_SIZE} />} type='button' className={'border p-2 mt-4'} onClick={addCredential} /> */}
+
         <MyDivider />
-        <MyButton
-          type='submit'
-          className={'mt-3'}
-          variant="primary">{t("Add & Save")}</MyButton>
+        <MyButton type="submit" className={'mt-3'} variant="primary">
+          {t('Add & Save')}
+        </MyButton>
       </form>
       <MyModal
         modalProps={{
           show: Boolean(openModal),
           onClose: () => setOpenModal(false),
-          size: 'md'
+          size: 'md',
         }}
         headerProps={{
-          children: <h2 className="text-gray-800">{t('Edit profile picture')}</h2>
+          children: <h2 className="text-gray-800">{t('Edit profile picture')}</h2>,
         }}
         bodyProps={{
           children: (
@@ -366,7 +294,7 @@ function Form() {
                 handleClose={() => setOpenModal(false)}
               />
             </>
-          )
+          ),
         }}
       />
     </>
